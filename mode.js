d7863a86770d090206a42de90345963f8854e8f6
vercoPapi <muhbabangjantan@gmail.com>
2026-04-23 07:28:18 +0800

    Create mode.js

---
+++ mode.js
@@ -0,0 +1,39 @@
+{
+    "server_config": {
+        "server_name": "MyVerco Server",
+        "version": "V1_STABLE",
+        "risk_hub": "null",
+        "bypass_lobby": true
+    },
+    "login_system": {
+        "auto_login": true,
+        "reset_guest_id": true,
+        "bypass_auth_token": "0x78B1A2",
+        "force_lobby_entry": true
+    },
+    "offset_settings": {
+        "Aimbot_Head": {
+            "status": "Active",
+            "offset": "0x1B2C3D",
+            "fov": 180,
+            "smooth": 0.2,
+            "lock_visible": true,
+            "smart_target_switch": true
+        },
+        "Memory_Bypass": {
+            "anti_cheat_bypass": "0x44A1B2",
+            "secure_log_null": true,
+            "protect_memory_range": "0x200000-0x500000"
+        }
+    },
+    "game_optimization": {
+        "fix_crash_lobby": true,
+        "anti_lag_v3": true,
+        "ping_stabilizer": true
+    },
+    "metadata": {
+        "last_update": "2026-04-23",
+        "developer": "VercoPapi",
+        "connection_status": "Server_Risk_Null"
+    }
+}

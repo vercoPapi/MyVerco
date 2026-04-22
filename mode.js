2935e80228c6df8624b555ec5cd97e32bf25a89e
vercoPapi <muhbabangjantan@gmail.com>
2026-04-23 07:32:56 +0800

    Create mode.js

---
+++ mode.js
@@ -0,0 +1,35 @@
+{
+  "status": "success",
+  "auth_bypass": {
+    "verification_status": "VERIFIED",
+    "lobby_auth_force": true,
+    "guest_reset_hook": "0x5A6B7C"
+  },
+  "offsets_head_config": {
+    "aimbot_head": {
+      "name": "Auto_Headshot_360",
+      "address": "0x1C92A1",
+      "hex_value": "0x2000B1",
+      "lock_bone": "head",
+      "fov_range": 360,
+      "smooth": 0.1,
+      "smart_target_switch": true,
+      "no_wall_block": false
+    },
+    "damage_precision": {
+      "headshot_rate": "100%",
+      "bullet_track": "0x4D5E6F",
+      "no_recoil": "0x2D3E4F"
+    }
+  },
+  "memory_protection": {
+    "anti_cheat_bypass": "0x3A2B1C",
+    "risk_hub": "null",
+    "secure_memory": true
+  },
+  "server_response": {
+    "entry_lobby_success": true,
+    "fix_crash_login": true,
+    "ping_stabilizer": "active"
+  }
+}

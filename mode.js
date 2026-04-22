c995f4393670f9fbd0d96a39e262d20e71aec2da
vercoPapi <muhbabangjantan@gmail.com>
2026-04-23 07:36:30 +0800

    Create mode.js

---
+++ mode.js
@@ -0,0 +1,30 @@
+{
+  "Version": "1.104.X",
+  "Auth": {
+    "Status": "Verified",
+    "Token": "BYPASS_SUCCESS",
+    "LobbyEntry": "Force",
+    "GuestReset": true
+  },
+  "Offsets": {
+    "Headshot": {
+      "Function": "AimLock_Head",
+      "Memory": "0x1C92A1",
+      "Value": "0x2000B1",
+      "Hook": "Bone_Head",
+      "FOV": 360,
+      "Smooth": 0.1,
+      "NoWall": true
+    },
+    "Bypass": {
+      "AntiCheat": "0x3A2B1C",
+      "LocalFix": "0x4D5E6F",
+      "NullHub": "Enabled"
+    }
+  },
+  "Global": {
+    "AntiCrash": true,
+    "FixLog": true,
+    "AutoEntry": true
+  }
+}
